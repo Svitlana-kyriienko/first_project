e3d26538ac305f2a8a1378388b3cfbb2cbecf073
Svitlana-kyriienko <1398525@gmail.com>
2025-10-21 13:23:54 +0200

    the tasks are complet completely

--- task1.py
+++ task1.py
@@ -13,38 +13,38 @@
 
 ##Двічі: Написати програму, яка зчитує ціле число та виводить його двічі.
 
-n3 = (int(input("Enter number: ")))
-print((str(n3) + " ") * 2)
+# n3 = (int(input("Enter number: ")))
+# print((str(n3) + " ") * 2)
 
 #Привіт, <ім'я>: Написати програму, яка запитує ім'я користувача і виводить привітання.
 
-name1 = input("Enter yor Name: ")
-print("Hallo, " + str(name1))
+# name1 = input("Enter yor Name: ")
+# print("Hallo, " + str(name1))
 
 #Прощавай, <ім'я>: Написати програму, яка запитує ім'я користувача і виводить прощання.
 
-name2 = input("Enter yor Name: ")
-print("Goodbye, " + str(name2))
+# name2 = input("Enter yor Name: ")
+# print("Goodbye, " + str(name2))
 
 #Вік через рік: Напишіть програму, яка запитує ім'я користувача та його вік. Потім виведіть повідомлення, яке повідомляє, скільки йому буде років через рік.
 
-name3 = input("Enter yor Name: ")
-age = int(input("Enter yor age: "))
-print(name3, ", ","in a year your age ", str(age + 1))
+# name3 = input("Enter yor Name: ")
+# age = int(input("Enter yor age: "))
+# print(name3, ", ","in a year your age ", str(age + 1))
 
 #Площа прямокутника: Напишіть програму, яка запитує довжину та ширину прямокутника, а потім обчислює та виводить його площу.
 
-lehgth = int(input("Enter the length: "))
-width = int(input("Enter the width: "))
-area = lehgth * width
-print("Area of a rectangle: ", str(area))
+# lehgth = int(input("Enter the length: "))
+# width = int(input("Enter the width: "))
+# area = lehgth * width
+# print("Area of a rectangle: ", str(area))
 
 #Конвертер валют: Запропонуйте користувачеві ввести суму в одній валюті (наприклад, доларах), а потім виведіть цю суму в іншій валюті (наприклад, євро), використовуючи заздалегідь визначений коефіцієнт обміну.
 
-usd = (int(input("Enter amount in usd: ")))
-k = 0.85
-eur = usd * k
-print("Amount in usd ", str(eur))
+# usd = (int(input("Enter amount in usd: ")))
+# k = 0.85
+# eur = usd * k
+# print("Amount in usd ", str(eur))
 
 #Вартість покупки: Запитайте користувача назву товару, його ціну та кількість. Обчисліть загальну вартість покупки та виведіть її на екран.
 
@@ -53,4 +53,7 @@ price = float(input("Enter the price: "))
 quantity = float(input("Enter the quantity: "))
 shopping = price * quantity
 print("Total purchase price")
+
 #Температура: Напишіть програму, яка перетворює температуру з градусів Цельсія на Фаренгейт. Запитайте користувача температуру в градусах Цельсія.
+
+#Ви придбали товар на певну суму s. Скільки купюр різного номіналу треба віддати продавцю, якщо починати платити з найбільших? У вас є 1, 2, 5, 10, 100, 500 гриве#нь.
--- task2home_work.py
+++ task2home_work.py
@@ -3,20 +3,50 @@
 #Задача 1. Середнє трьох чисел
 #Користувач вводить три числа. Обчисли середнє арифметичне.
 
+n4 = int(input("Enter number1: "))
+n5 = int(input("Enter number2: "))
+n6 = int(input("Enter number3: "))
+print ((n4 + n5 + n6) / 3)
+
 #Задача 2. Остача від ділення
 #Користувач вводить два числа. Знайди остачу від ділення першого на друге.
+
+n4 = int(input("Enter number1: "))
+n5 = int(input("Enter number2: "))
+n8 = int(input("Enter number2: "))
+print(n7 % n8)
+
 
 #Задача 3. Подвоєне число
 #Користувач вводить число. Виведи подвоєне значення цього числа.
 
+n9 = float(input("Enter number: "))
+print (n9 * 2)
+
 #Задача 4. Конвертація хвилин у секунди
 #Користувач вводить кількість хвилин. Обчисли, скільки це буде секунд.
+
+minutes = int(input("Enter minutes: "))
+print("It's in sekonds ", minutes * 60)
 
 #Задача 5. Кількість яблук на кожного
 #Є n яблук і k учнів. Скільки яблук отримає кожен учень, якщо ділити порівну, і скільки залишиться?
 
+n = int(input("Enter the number of apples: "))
+k = int(input("Enter the number of students: "))
+print("Every studet will receive apples ", n // k)
+print("The rest of the apples ", n % k)
+
 #Задача 6. Остання цифра числа
 #Користувач вводить число. Виведи його останню цифру.
 
+n10 = int(input("Enter number: "))
+print(n10 % 10)
+
 #Задача 7*. Обмін змінних
 #Користувач вводить два числа. Після цього потрібно “поміняти” їх місцями і вивести результат.
+
+n11 = int(input("Enter number1: "))
+n12 = int(input("Enter number2: "))
+n11, n12 = n12, n11
+print(n11, n12)
